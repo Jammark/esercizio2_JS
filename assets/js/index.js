f1d7ef007ce63f77e72d845ec911ab288a97d184
marcodinicola88 <marco.dinicola@yahoo.it>
2023-04-04 14:41:36 +0200

    commit numero 1

---
+++ assets/js/index.js
@@ -0,0 +1,50 @@
+//es 1
+// tipo numerico sia intero (rappresentano i numeri interi) 
+var numeroInntero = 3;
+// sia decimale (rappresentano i numeri frazionari)
+var numeroDecimale = 1.2;
+// tipo stringa
+// rappresentano una sequenza di caratteri
+var stringa1 ='esempio';
+var stringa2 = "esempio";
+// tipo booleano
+// rappresenta una condizione e assume valore true o false
+var booleano = true;
+// tipo null 
+// mancanza di identificazione, valore appositamente assente
+// rappresenta un valore unico per variabili con valore assente
+var nulla = null;
+// tipo undefined
+// indica valore non assegnato o variabile non definnita
+var indefinita = undefined;
+
+//es 2
+var name = "Marco";
+
+//es 3
+var somma = 12;
+12 += 20;
+
+//es 4
+var x;
+x = 12;
+
+//es5 
+name = "Di Nicola";
+const costante = 5;
+costante = 4;
+console.log('costante con valore 5 :' + costante);
+
+//es 6
+
+x = 4 - x;
+
+
+//es7
+
+var name1, name2;
+name1 = "jhon";
+name2 = "Jhon";
+
+console.log('jhon == Jhon: '+name1 == name2);
+console.log('jhon == Jhon to lowercase: '+name1 == name2.toLowerCase);
